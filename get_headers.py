abaa996a63fc684fe72d5c6724d73efc2cc0bca6
chaimo <84918212+yaoshunhuai@users.noreply.github.com>
2025-08-06 22:03:11 +0800

    1

--- get_headers.py
+++ get_headers.py
@@ -6,10 +6,12 @@ from seleniumwire import webdriver
 
 def get_request_headers():
     os.system(f'set driver_path="{os.getcwd()}"')
+
     """
     服网上几种设置WebDriver路径都没用, 还得自己探索下, 设置临时环境变量解决了
     翻翻 Python312\Lib\site-packages\selenium\webdriver\common\driver_finder.py 看到"driver_path"就试试没想到成功了
     """
+
     if input('是否需要添加证书(推荐,减少ssl安全问题|需要管理员权限,请使用管理员权限运行):(Y/n)').lower() == 'y':
         os.system(f'certutil -addstore root {os.getcwd()}\ca.crt')
 
